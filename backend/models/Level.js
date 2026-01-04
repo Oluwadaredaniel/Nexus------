1205074e402c552d222b432f6824382cbf73c3ba
Oluwadaredaniel <oluwadare458@gmail.com>
2026-01-04 08:47:05 +0100

    Update Level.js

--- backend/models/Level.js
+++ backend/models/Level.js
@@ -1,6 +1,8 @@
 
 import mongoose from 'mongoose';
+
 const levelSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true } 
+  name: { type: String, required: true, unique: true } // e.g., "100", "200", "300", "400", "500", "Spill"
 }, { timestamps: true });
+
 export default mongoose.model('Level', levelSchema);
